346d658e0256f4a0b1aa3b6fed78f0cdc8b0e7c9
Mindaugas Ulonas <mulonas@Mindaugass-MacBook-Air.local>
2023-03-21 19:59:26 +0200

    adding clarifai

--- magicbrainlearning7/src/App.js
+++ magicbrainlearning7/src/App.js
@@ -4,7 +4,15 @@ import Navigation from './Components/Navigation/Navigation';
 import BrainImage from './Components/BrainImage/BrainImage';
 import UserRank from './Components/UserRank/UserRank';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+
+window.process = {};
+
+const app = new Clarifai.App({
+  apiKey: '82819b6c8d2d4417abbdebb80e6a3cdc'
+});
 
 class App extends Component {
   constructor() {
@@ -16,11 +24,14 @@ class App extends Component {
   };
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   };
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageURL: this.state.input});
+    app.models.predict(Clarifai.CELEBRITY_MODEL, this.state.input)
+    .then(response => console.log(response.outputs[0].data.regions[0].region_info.bounding_box))
+    .catch(error => console.log(error))
   };
 
   render() {
@@ -31,6 +42,7 @@ class App extends Component {
         <BrainImage />
         <UserRank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageURL={this.state.imageURL} />
       </div>
     )
   }
