49e2453d2dd8b4abf5fd4d20936538d95ee70e0e
Mindaugas Ulonas <mindegi15@gmail.com>
2023-04-04 17:22:30 +0300

    adding navigation

--- magicbrainlearning7/src/Components/Navigation/Navigation.js
+++ magicbrainlearning7/src/Components/Navigation/Navigation.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = ({ onChangeRouteClick }) => {
-    return (
-        <div className='navigationMoveToLeft'>
-            <p onClick={() => onChangeRouteClick('signin')} className='f3 black grow br3 shadow-5 ma4 pa3 navigationMaxWidth pointer'>Sign Out</p>
-        </div>
-    )
+const Navigation = ({ onChangeRouteClick, isSignedIn }) => {
+    if (isSignedIn) {
+        return (
+            <div className='navigationMoveToLeft'>
+                <p onClick={() => onChangeRouteClick('signin')} className='f3 black grow br3 shadow-5 ma4 pa3 navigationMaxWidth pointer'>Sign Out</p>
+            </div>
+        )
+            } else {
+                return (
+                    <div className='navigationMoveToLeft'>
+                        <p onClick={() => onChangeRouteClick('signin')} className='f3 black grow br3 shadow-5 ma4 pa3 navigationMaxWidth pointer'>Sign In</p>
+                        <p onClick={() => onChangeRouteClick('register')} className='f3 black grow br3 shadow-5 ma4 pa3 navigationMaxWidth pointer'>Register</p>
+                    </div>
+                )
+            }
 }
 
 export default Navigation;
