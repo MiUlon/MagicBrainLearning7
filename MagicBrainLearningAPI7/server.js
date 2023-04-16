5729e8a4067bf58cb29b7739052c9c7e8667ee60
Mindaugas Ulonas <mindegi15@gmail.com>
2023-04-16 12:59:13 +0300

    connecting DB to project

--- MagicBrainLearningAPI7/server.js
+++ MagicBrainLearningAPI7/server.js
@@ -2,6 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
+import knex from 'knex';
+
+const postgresDB = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        port : 5432,
+        user : 'postgres',
+        password : 'test',
+        database : 'MagicBrainLearning7'
+    }
+});
 
 const app = express();
 
--- magicbrainlearning7/src/App.js
+++ magicbrainlearning7/src/App.js
@@ -29,7 +29,6 @@ class App extends Component {
         id: '',
         name: '',
         email: '',
-        password: '',
         entries: 0,
         joined: ''
       }
